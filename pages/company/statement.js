1c45f497d9dbda56f1bab7978880e71bc1957939
route <route@routeMBP.local>
2021-03-10 22:24:20 +0900

    create media and statement pages

--- pages/company/statement.js
+++ pages/company/statement.js
@@ -12,51 +12,106 @@ export default function Statement() {
       </Head>
       <Layout>
       <main>
-          <div style={{backgroundColor: "#786b58"}}>
-            <div class="mx-auto py-2 bg-opacity-75" style={{ width: "1100px" }}>
-              <div class="text-white mx-10">
-                <div>
-                  <h1 class="text-4xl text-center border-b-2 mb-6 mt-2 pb-2">VISION</h1>
+        <div style={{backgroundColor: "#786b58"}}>
+          <div class="mx-auto pt-8 pb-16 bg-opacity-75" style={{ width: "1100px" }}>
+            <div class="text-white mx-40">
+              <div>
+                <h1 class="text-4xl text-center border-b-4 pb-2">STATEMENT</h1>
+              </div>
+              <div class="text-center tracking-widest mt-16" >
+                <h1 class="text-3xl mb-8 font-bold">最高品質の戦略的モノ作りカンパニー</h1>
+                <div class="text-xl leading-8">
+                  <p>市場の求めているモノ作り。市場を創造するモノ作り。</p>
+                  <p>生産から顧客納品まで、全てを自社一括管理。</p>
+                  <p>変わり続ける時代と需要に応え続ける企業として存在します。</p>
                 </div>
-                <div class="px-56 text-center" >
-                  <h1 class="text-2xl mb-8 font-bold tracking-widest">今日よりも、もっと幸せな明日を。</h1>
-                  <div class="mb-8">
-                    <p>高品質かつワクワクする商品を生み出し、</p>
-                    <p class="mt-2">お客様の生活をより豊かで幸せなものにすること。</p>
-                  </div>
-                  <div class="mb-8">
-                    <p>お客様だけでなく、社会・社員・関係各社、</p>
-                    <p class="mt-2">関わる全ての人々の幸せと発展に貢献すること。</p>
-                  </div>
-                  <div class="mb-8">
-                    <p>地球環境の保全を心がけ、永続的な発展をするために</p>
-                    <p class="mt-2">無駄のない流通・運営を行い、毎日の成長を続けること。</p>
+              </div>
+            </div>
+
+            <div class="text-white mx-40 mt-24">
+              <div>
+                <h1 class="text-4xl text-center border-b-4 pb-2">VISION</h1>
+              </div>
+              <div class="text-center tracking-widest mt-5" >
+                <h1 class="text-3xl mb-8 font-bold">今日よりも、もっと幸せな明日を。</h1>
+                <div class="mb-8 text-xl leading-8">
+                  <p>高品質かつワクワクする商品を生み出し、</p>
+                  <p>お客様の生活をより豊かで幸せなものにすること。</p>
+                </div>
+                <div class="mb-8 text-xl leading-8">
+                  <p>お客様だけでなく、社会・社員・関係各社、</p>
+                  <p>関わる全ての人々の幸せと発展に貢献すること。</p>
+                </div>
+                <div class="mb-8 text-xl leading-8">
+                  <p>地球環境の保全を心がけ、永続的な発展をするために</p>
+                  <p>無駄のない流通・運営を行い、毎日の成長を続けること。</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="text-white mx-40 mt-24">
+              <div>
+                <h1 class="text-3xl text-center font-bold border-b-4 pb-2">代表メッセージ</h1>
+              </div>
+              <div class="text-center tracking-widest mt-16" >
+                <h1 class="text-2xl mb-6 font-bold">先人達を尊び、感謝をし、天命を尽くす</h1>
+                <div class="text-left">
+                  <div class="mb-8 text-xl leading-8">
+                    <p>私たちがこうして平和に暮らせるのは、平和で美しい世界、そして日本を作るために生きてきた多くの偉大な先人達のおかげです。</p>
+                    <p>自分だけの利益や安定、安全に執着するのではなく、先人達を尊び、感謝をして、後世の人たちが綺麗な地球で平和に暮らしていくために、毎日天命を尽くしましょう。</p>
                   </div>
                 </div>
               </div>
-              
-              <div class="bg-white mt-10 mb-2 mx-10">
-                <div><h1 class="text-2xl text-center border-b-4  mx-10 pt-6 pb-4 font-semibold" style={{borderColor: "#786b58"}}>代表メッセージ</h1></div>
-                <div class="text-center flex flex-row my-10 mx-10">
-                  <div class="w-1/5">
-                    <Image src="/images/company/ceo_img.jpg" width={120} height={120} alt="ceo_img"/>
-                    <p>代表者 Profile</p>
+
+              <div class="text-center tracking-widest mt-16" >
+                <h1 class="text-2xl mb-6 font-bold">「照于一隅」一隅を照らす</h1>
+                <div class="text-left">
+                  <div class="mb-8 text-xl leading-8">
+                    <p>天台宗開祖の最澄禅師は著書『山家学生式』の中で「国の宝とはなにか。 宝とは道を修めようとする心である。 この道心をもっている人こそ、 社会にとって、なくてはならない国の宝である。 社会の一隅にいながら、社会を照らす生活をする。その人こそが、なくてはならない国宝の人である。」と記しています。</p>
+                    <p>それぞれの立場で最善を尽くし、ひとりひとりの内なる灯火が周囲に広がれば、個人から会社、ひいては社会全体が明るく照らされると私は信じています。</p>
                   </div>
-                  <div class="w-4/5">
-                    <div class="pr-56 text-left font-medium tracking-wider">
-                      <p class="mb-6">毎月仕事で様々な国を訪れて解った事があります。日本人の多くはとにかく顔が暗いのです。</p>
-                      <p class="mb-6">日本の長い歴史を振り返ってもどれほどの期間が天下太平だったのでしょう。衣食住・衛生・治安的に安定した生活がほとんどの人に与えられてる今なぜこんなにも日本人の心や顔は暗いのでしょうか。</p>
-                      <p class="mb-6">現代の日本社会は物質的に豊かな時代になったと言えるでしょう。しかしその一方で、情報や生き方が多様・複雑化する中で何を信念に生きていくかが分からなくなっているのではないでしょうか。</p>
-                      <p class="mb-6">望まぬ戦争に巻き込まれた多くの人の血の上に今の平和な日本や世界各国が築かれ、そしてまだ世界では生命の担保さえ出来ない国がたくさんあります。</p>
-                      <p class="mb-6">その中で私たちがこうして平和に暮らせるのは、平和で美しい世界、そして日本を作るために生きてきた多くの偉大な先人達のおかげです。</p>
-                      <p class="mb-6">このような人達が毎日暗い顔をして生きていたり、我先に利益や自分個人の安定や安全に執着して生きている多くの日本人を見たら、どの様な気持ちでしょうか？</p>
-                      <p class="mb-6">私たちはその様な時代を生き抜いた先人達を尊び、感謝をして、後世の人たちが綺麗な地球で平和に暮らしていくために、毎日天命を尽くすしかないのではないでしょうか。</p>
-                    </div>
+                </div>
+              </div>
+
+              <div class="text-center tracking-widest mt-16" >
+                <div class="text-2xl mb-6 font-bold">
+                  <h1>強くなければ生きていけない、</h1>
+                  <h1>優しくなければ生きていく資格がない</h1>
+                  <h1>愛なき力は暴力、力なき愛は無力である。</h1>
+                </div>
+                <div class="text-left">
+                  <div class="mb-8 text-xl leading-8">
+                    <p>多くの人は幻の幸せや安定にしがみつきたくなりがちですが、人生の真の拠り所になるのは個人個人の魂の中にしかありません。会社にしても一生安泰のビジネスモデルはありません。</p>
+                    <p>私たちは、不変的な人生の真理を掴みながら、刻々と変化する世界情勢やマーケットを捉え常に優しさを持って対応していきます。</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="text-center tracking-widest mt-16 pb-32" >
+                <h1 class="text-2xl mb-6 font-bold">道心の中に衣食あり、衣食の中に道心なし</h1>
+                <div class="text-left">
+                  <div class="mb-8 text-xl leading-8">
+                    <p>道心がなければ、いくら恵まれても無意味です。道心があればこそ恵まれた心と生活になると私は思います。真剣に道を求め、その道に打ち込む人は、 自ずと生活が成り立ち、必要な衣食住も備わります。しかし、衣食住に執着し、ぜいたく三昧の生活を志向する人は、私欲に心が奪われて仕事もなおざりとなってしまいます。</p>
+                    <p>私も会社もまだ道の途中です。みなさまと出会い、お互いに学び、成長することで道心を極めていきたいと思います。</p>
                   </div>
                 </div>
               </div>
             </div>
+
+            <div class="flex flex-row mx-40" style={{ width: "1100px" }}>
+              <div>
+                <Image src="/images/company/ceo_img.jpg" width={240} height={240} alt="ceo_img"/>
+              </div>
+              <div class="text-lg text-white pt-6 pl-10">
+                <p>代表プロフィール</p>
+                <p class="text-xl py-6">清水真輝</p>
+                <p class="text-lg pb-6">1987年11月27日生まれ AB型</p>
+                <p class="text-lg">名古屋市出身</p>
+                <p class="text-lg">愛知高校・名古屋学院大学英米科卒業</p>
+              </div>
+            </div>
           </div>
+        </div>
       </main>
       </Layout>
     </div>
--- pages/media/index.js
+++ pages/media/index.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
+
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import InstagramEmbed from 'react-instagram-embed';
 
 import Layout from '../../components/layout'
 
-export default function Media() {
+export default function Media({videos}) {
+  const appID = '3888350634542239';
+  const clientToken = '07d4a459f6430871cf1590f093d3069d';
+  const clientAccessToken = `${appID}|${clientToken}`
+
   return (
     <div>
       <Head>
@@ -11,12 +21,139 @@ export default function Media() {
       </Head>
       <Layout>
       <main>
-        <div class="mx-auto pt-12 mb-12" style={{ width: "1100px" }}>
-          <div><h1 class="text-4xl text-center mb-6">MEDIA</h1></div>
-          <div><h1 class="text-xl text-center mb-6">coming soon</h1></div>
+        <div style={{backgroundColor: "#786b58"}}>
+          <div class="mx-auto pt-12 pb-12" style={{ width: "1100px" }}>
+            {/*見出し部分*/}
+            <div class="flex pb-2 border-b-2 border-solid border-white mb-12">
+              <div class="w-1/4"><p>{`　`}</p></div>
+              <div class="w-2/4">
+                <h1 class="text-4xl text-center text-white">MEDIA</h1>
+                </div>
+              <div class="w-1/4">
+                <div class="float-right mr-4">
+                  <div class="inline-block">
+                    <a href="https://www.instagram.com/masaki.s_japan/">
+                      <Image src="/images/media/instagram_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" />
+                    </a>
+                  </div>
+                  <div class="inline-block"><Image src="/images/media/facebook_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                  <div class="inline-block"><Image src="/images/media/youtube_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                  <div class="inline-block"><Image src="/images/media/twitter_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                </div>
+              </div>
+            </div>
+            {/*見出し部分 終了*/}
+            {/*内容*/}
+            <div class="px-6 pt-8 bg-white border-2 border-solid border-black">
+              <section class="mb-24">{/*RadioSection*/}
+                <h1 class="text-5xl text-center mb-6 midashiGo">RADIO</h1>
+                <div class="mb-6">
+                  <Image src="/images/media/bond_jam.jpg" alt="radio-image" width={1100} height={227} />
+                </div>
+                <div>
+                  <p class="chuGo text-2xl mb-4">MC: NANAMI 清水真輝</p>
+                  <p class="chuGo text-2xl mb-4">BACHIC ASHLEY がプロデュースする日常を最高の気分で過ごすために毎日を日々、</p>
+                  <p class="chuGo text-2xl mb-4">力強く生きているアーティスト、クリエイター、経営者など、各界の最先端で活躍する方々を</p>
+                  <p class="chuGo text-2xl mb-6">ゲストとしてお招きし、その強さの秘訣とこだわりの人生についてお聞きします。</p>
+                  <p class="chuGo text-xl">FM 愛知　毎週20：00～20：30　OA</p>
+                </div>
+              </section>{/*RadioSectionEnd*/}
+              <section class="mb-24">{/*YoutubeSection*/}
+                <div class="flex justify-center mb-6">
+                  <div class="flex items-center">
+                    <h1 class="text-5xl text-center mr-6 midashiGo">Youtube Channel</h1>
+                    <Image src="/images/media/youtube-full-logo_lg.png" width={150} height={34} alt="youtubelogo"/>
+                  </div>
+                </div>
+                {videos.map(video=>{
+                  console.log(video.id)
+                })}
+                <div class="flex pt-6 pb-6 mb-4" style={{ backgroundColor: "#221916" }}>
+                  <div class="w-2/4">
+                    <div class="flex justify-center items-center h-full">
+                      <iframe width="95%" height="95%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                  </div>
+                  <div class="w-1/4">
+                    <div class="flex justify-center items-center h-1/2">
+                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="flex justify-center items-center h-1/2">
+                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                  </div>
+                  <div class="w-1/4">
+                    <div class="flex justify-center items-center h-1/2">
+                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div class="flex justify-center items-center h-1/2">
+                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <p class="chuGo text-2xl mb-4">BACHIC ASHLEY がプロデュースするラジオ番組「Bond Jam」のアーカイブからブランド、</p>
+                  <p class="chuGo text-2xl mb-4">企業情報まで株式会社ファルコナーの魅力をYouTube コンテンツで発信します。</p>
+                </div>
+              </section>{/*YoutubeSectionEnd*/}
+              <section class="mb-24">{/*SNSSection*/}
+                <h1 class="text-6xl text-center mr-6 midashiGo mb-16">SNS</h1>
+                <div class="flex">
+                  <div class="w-1/2 pl-12">
+                    <div class="flex justify-start">
+                      <Image class="block" src="/images/media/instagram_logo_1500.png" width={40} height={40} alt="twitter_logo" />
+                    </div>
+                    <div class="mt-6">
+                      <InstagramEmbed
+                        url='https://www.instagram.com/p/CMO-EpVgQDh/'
+                        clientAccessToken={clientAccessToken}
+                        maxWidth={400}
+                        hideCaption={true}
+                        containerTagName='div'
+                        protocol=''
+                        injectScript
+                        onLoading={() => {}}
+                        onSuccess={() => {}}
+                        onAfterRender={() => {}}
+                        onFailure={() => {}}
+                      />
+                    </div>
+                  </div>
+                  <div class="w-1/2 pr-12">
+                    <div class="flex justify-end">
+                      <Image class="block" src="/images/media/Twitter_Logo_WhiteOnBlue_400.png" width={40} height={40} alt="twitter_logo" />
+                    </div>
+                    <div class="flex justify-end mt-6">
+                      <div style={{ width: "400px" }}>
+                        <TwitterTimelineEmbed
+                          sourceType="profile"
+                          screenName="Nanami_Bondjam"
+                          options={{height: 600}}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>{/*SNSSectionEnd*/}
+            </div>
+          </div>{/*コンテナー終了*/}
         </div>
       </main>
       </Layout>
     </div>
   )
+}
+
+export const getStaticProps = async() => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://falconer.microcms.io/api/v1/youtube', key)
+    .then(res => res.json())
+    .catch(() => null);
+  return {
+    props: {
+      videos: data.contents,
+    },
+  };
 }
