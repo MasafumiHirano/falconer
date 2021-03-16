a0071d5a1cf2c370cc7ee1eb086c6d71522d3393
katsuhiko-ranku <k.fuse@ranku.jp>
2021-03-16 12:30:19 +0900

    レスポンシブ対応

--- pages/company/index.js
+++ pages/company/index.js
@@ -23,20 +23,21 @@ export default function Company() {
           </div>
           {/* BUSINESS/PRODUCTS 開始 */}
           <div class="py-12" style={{backgroundColor: "#cecece" }}>
-            <div class="mx-auto" style={{width: "920px"}}>
-              <div><h1 class="text-4xl text-center mb-6">BUSINESS / PRODUCTS</h1></div>
-              <div class="flex">
+            <div class="mx-auto lg:w-1100 px-4 lg:px-20">
+              <h1 class="flex justify-center text-4xl text-center mb-6">BUSINESS / PRODUCTS</h1>
+              <div class="flex justify-center">
                 <div class="pb-4" style={{ width: "450px", marginRight: "20px" }}>
                   <div class="relative">
                     <div
-                      class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
                       style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
                     >
                       レザーアイテム
                     </div>
                     <Image src="/images/company/prod01.png" width={450} height={300} />
                   </div>
-                  <p>イタリアのなめし手法を用いたイタリアンレザーを中心にグラデーションレザーなど独自の素材を抱え、
+                  <p class="text-xs lg:text-base">
+                    イタリアのなめし手法を用いたイタリアンレザーを中心にグラデーションレザーなど独自の素材を抱え、
                     小売/卸売事業の中～高単価商材として財布やバッグなどのレザー商品を提供します。
                     マーケットの需要を予測するだけでなく、潜在需要の掘り起こしを戦略的に行い、
                     ずっと使えるモノ作りを続けています。
@@ -45,74 +46,95 @@ export default function Company() {
                 <div class="pb-4" style={{ width: "450px"}}>
                 <div class="relative">
                   <div
-                    class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
-                    style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
+                      style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
                   >
                     OEM/ODM 事業
                   </div>
                   <Image src="/images/company/prod02.png" width={450} height={300} />
                   </div>
-                  <p>自社ブランドの運営・運用の経験をベースに、マーケット調査から製造・販売まで全ての独自技術と能力を結集。
+                  <p class="text-xs lg:text-base">
+                    自社ブランドの運営・運用の経験をベースに、マーケット調査から製造・販売まで全ての独自技術と能力を結集。
                     新たな商品群の開発から既存製品のブラッシュアップまで、弊社にしかできないモノ作りを提供します。
                   </p>
                 </div>
               </div>
-              <div class="flex">
-                <div class="pb-4 relative" style={{ width: "450px", marginRight: "20px" }}>
+              <div class="flex justify-center">
+                <div class="pb-4 relative w-450" style={{marginRight: "20px" }}>
                   <div class="relative">
                     <div
-                      class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
                       style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
                     >
                       電化製品
                     </div>
                     <Image src="/images/company/prod03.png" width={450} height={300} />
                   </div>
-                  <p>私たちが日々暮らしていく中で必要不可欠な電化製品。最近は生活に豊かな彩りを加えるためにAIを取り入れたり、
+                  <p class="text-xs lg:text-base">
+                    私たちが日々暮らしていく中で必要不可欠な電化製品。最近は生活に豊かな彩りを加えるためにAIを取り入れたり、
                     ネットワークと接続し、音声認識で指示を出したり。より直感的でシームレスな製品の開発・販売を行っております。
                   </p>
                 </div>
-                <div class="pb-4" style={{ width: "450px"}}>
+                <div class="pb-4 w-450">
                   <div class="relative">
                     <div
-                      class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
                       style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
                     >
                       電子タバコ用品
                     </div>
                     <Image src="/images/company/prod04.png" width={450} height={300} />
                   </div>
-                  <p>今マーケットが欲しているモノを戦略的に市場にリリース。ユーザーの信頼感を意識した自社ブランド展開を行っております。
+                  <p class="text-xs lg:text-base">
+                    今マーケットが欲しているモノを戦略的に市場にリリース。ユーザーの信頼感を意識した自社ブランド展開を行っております。
                     PUレザー、本革、牛革、PC、TPUなどの複数の素材を採用し、小売/卸事業の低単価高回転商材として提供します。
                   </p>
                 </div>
               </div>
-              <div class="flex">
-                <div class="pb-4" style={{ width: "450px", marginRight: "20px" }}>
+              <div class="flex justify-center">
+                <div class="pb-4 relative w-450" style={{marginRight: "20px" }}>
                   <div class="relative">
                     <div
-                      class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
+                      style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
+                    >
+                      電化製品
+                    </div>
+                    <Image src="/images/company/prod03.png" width={450} height={300} />
+                  </div>
+                  <p class="text-xs lg:text-base">
+                    私たちが日々暮らしていく中で必要不可欠な電化製品。最近は生活に豊かな彩りを加えるためにAIを取り入れたり、
+                    ネットワークと接続し、音声認識で指示を出したり。より直感的でシームレスな製品の開発・販売を行っております。
+                  </p>
+                </div>
+
+                {/* <div class="pb-4 w-450" style={{marginRight: "20px" }}>
+                  <div class="relative">
+                    <div
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
                       style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
                     >
                       アパレル
                     </div>
-                  <div style={{ width: "450px", height: "300px", backgroundColor: "#241916"}}></div>
+                    <div style={{ width: "450px", height: "300px", backgroundColor: "#241916"}}></div>
                   </div>
-                  <p class="mt-1">私たちが日々暮らしていく中で必要不可欠な電化製品。最近は生活に豊かな彩りを加えるためにAIを取り入れたり、
+                  <p class="text-xs lg:text-base mt-1">
+                    私たちが日々暮らしていく中で必要不可欠な電化製品。最近は生活に豊かな彩りを加えるためにAIを取り入れたり、
                     ネットワークと接続し、音声認識で指示を出したり。より直感的でシームレスな製品の開発・販売を行っております。
                   </p>
-                </div>
-                <div class="pb-4" style={{ width: "450px"}}>
-                <div class="relative">
-                  <div
-                    class="absolute z-10 text-white py-2 px-4 border border-solid border-white font-bold bg-black tracking-widest"
-                    style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
-                  >
-                    海外事業
+                </div> */}
+                <div class="pb-4 w-450">
+                  <div class="relative">
+                    <div
+                      class="absolute z-10 text-white text-xs lg:text-base py-2 px-2 lg:px-4 font-bold bg-black border border-solid border-white tracking-widest"
+                      style={{backgroundColor: "rgba(0,0,0,0.3)", top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
+                    >
+                      海外事業
+                    </div>
+                    <Image src="/images/company/prod05.png" width={450} height={300} />
                   </div>
-                  <Image src="/images/company/prod05.png" width={450} height={300} />
-                  </div>
-                  <p>今マーケットが欲しているモノを戦略的に市場にリリース。ユーザーの信頼感を意識した自社ブランド展開を行っております。
+                  <p class="text-xs lg:text-base">
+                    今マーケットが欲しているモノを戦略的に市場にリリース。ユーザーの信頼感を意識した自社ブランド展開を行っております。
                     PUレザー、本革、牛革、PC、TPUなどの複数の素材を採用し、小売/卸事業の低単価高回転商材として提供します。
                   </p>
                 </div>
@@ -120,9 +142,10 @@ export default function Company() {
             </div>
           </div>
           {/* BUSINESS/PRODUCTS 終了 */}
-          <div class="mx-auto pt-6 mb-6" style={{ width: "920px" }}>
-          <Link href="/company/statement"><a>
-            <div class="relative">
+
+          <div class="mx-auto pt-6 mb-6 lg:w-1100 px-4 lg:px-20">
+            <Link href="/company/statement"><a>
+            <div class="relative flex justify-center">
               <div class="absolute text-center z-10" style={{top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}>
                 <div class="text-4xl text-white futura">STATEMENT</div>
                 <div class="mt-6 text-2xl text-white">代表メッセージ</div>
@@ -131,59 +154,48 @@ export default function Company() {
             </div>
             </a></Link>
           </div>
-          <div class="mx-auto mb-6" style={{ width: "920px" }}>
-            <div class="flex">
-              
-                <div class="mb-4 border-4 border-solid border-black" style={{ width: "450px", marginRight: "20px" }}>
-                  <div class="relative">
-                    <div
-                      class="absolute z-10 text-2xl font-bold tracking-widest"
-                      style={{top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
-                    >
-                      企業概要
-                    </div>
-                  <div style={{ width: "450px", height: "300px"}}></div>
+
+          <div class="mx-auto mb-6 lg:w-1100 px-4 lg:px-20">
+            <div class="flex justify-center mb-6">
+              <div class="border-4 border-solid border-black w-450 h-300 flex justify-center items-center" style={{marginRight: "20px" }}>
+                <div class="text-2xl font-bold tracking-widest">企業概要</div>
+              </div>
+              <div class="border-4 border-solid border-black w-450 h-300 flex justify-center items-center">
+                <div class="text-2xl font-bold tracking-widest">採用情報</div>
+              </div>
+            </div>
+          </div>
+          <div class="mx-auto mb-6 lg:w-1100 px-4 lg:px-20 flex justify-center">
+            <div class="flex justify-center w-920" style={{backgroundColor: "#241916"}}>
+              <div class="py-6 px-6 mb-6">
+                <h1 class="text-4xl text-white text-center mb-2">MEDIA</h1>
+                <div class="flex justify-center">
+                  <div class="inline-block">
+                    <a href="https://www.instagram.com/masaki.s_japan/">
+                      <Image src="/images/media/instagram_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" />
+                    </a>
                   </div>
+                  <div class="inline-block"><Image src="/images/media/facebook_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                  <div class="inline-block"><Image src="/images/media/youtube_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                  <div class="inline-block"><Image src="/images/media/twitter_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
                 </div>
-               
-              <div class="mb-4 border-4 border-solid border-black" style={{ width: "450px"}}>
-                <div class="relative">
-                  <div
-                    class="absolute z-10 text-2xl font-bold tracking-widest"
-                    style={{top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
-                  >
-                    採用情報
+                <div class="flex justify-center">
+                  <div class="mx-auto md:w-720 md:h-405">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/k5oSVWFyOqE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                   </div>
-                <div style={{ width: "450px", height: "300px"}}></div>
                 </div>
               </div>
             </div>
-            </div>
-            <div class="mx-auto py-6 mb-6" style={{ width: "920px", backgroundColor: "#241916"}}>
-              <div><h1 class="text-4xl text-white text-center mb-2">MEDIA</h1></div>
+          </div>
+
+          <div class="mx-auto mb-12 lg:w-1100 px-4 lg:px-20">
               <div class="flex justify-center">
-                <div class="inline-block">
-                  <a href="https://www.instagram.com/masaki.s_japan/">
-                    <Image src="/images/media/instagram_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" />
-                  </a>
-                </div>
-                <div class="inline-block"><Image src="/images/media/facebook_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
-                <div class="inline-block"><Image src="/images/media/youtube_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
-                <div class="inline-block"><Image src="/images/media/twitter_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
-              </div>
-              <iframe class="mx-auto" width="720" height="405" src="https://www.youtube.com/embed/k5oSVWFyOqE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <div class="mx-auto mb-12 border-4 border-solid border-black" style={{ width: "920px"}}>
-                <div class="relative">
-                  <div
-                    class="absolute z-10 text-2xl font-bold tracking-widest"
-                    style={{top:"50%", left:"50%", transform: "translate(-50%,-50%)"}}
-                  >
-                    お問い合わせ
-                  </div>
-                <div style={{ width: "920px", height: "150px"}}></div>
+                <div class="border-4 border-solid border-black flex justify-center items-center w-full" style={{ width: "920px", height: "150px"}}>
+                  <div class="text-2xl font-bold tracking-widest">お問い合わせ</div>
                 </div>
               </div>
+          </div>
+
         </main>
       </Layout>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({topics}) {
       <Layout>
         <main>
           <div class="bg-black">
-            <div class="mx-auto" style={{ width: "1280px", height: "640px" }}>
+            <div class="mx-auto">
               <Slider {...settings}>
                 <div>
                   <Image class="block" src="/images/topslider/bachicashley_hero.jpg" width={1280} height={640} alt="bachicashley_hero"/>
@@ -54,11 +54,12 @@ export default function Home({topics}) {
               </Slider>
             </div>
           </div>
+
           <div style={{backgroundColor: "#cecece" }}>
-            <div class="mx-auto py-12" style={{ width: "1100px" }}>
-              <div><h1 class="text-4xl text-center mb-6" style={{fontFamily: "futura_light_bt"}}>TOPICS</h1></div>
+            <div class="mx-auto py-8 lg:py-12 lg:w-1100">
+              <h1 class="text-4xl text-center" style={{fontFamily: "futura_light_bt"}}>TOPICS</h1>
               <div>
-                <ul class="grid grid-cols-3 gap-x-4 gap-y-8 mt-12">
+                <ul class="grid grid-cols-3 gap-x-3 lg:gap-x-4 gap-y-8 lg:gap-y-16 mt-12">
                   {topics.filter(topic => (
                     topic.topview == true
                   ))
@@ -69,10 +70,12 @@ export default function Home({topics}) {
                           <div>
                             <div><Image src={`${topic.main_image.url}`} width={360} height={210} alt=""/></div>
                             <div class="h-24">
-                              <div class="text-xl font-bold mb-4">{topic.title}</div>
-                              <div class="flex mb-12">
+                              <div class="lg:text-xl font-bold lg:mb-4">{topic.title}</div>
+                              <div class="lg:flex">
                                 {topic.tag.map(tag => (
-                                  <span class="px-4 py-2 mr-2 bg-white rounded-full text-xs">{tag}</span>
+                                  <div class="block">
+                                    <span class="px-2 lg:px-4 lg:py-2 mr-2 mb-2 bg-white rounded-full text-xs">{tag}</span>
+                                  </div>
                                 ))}
                               </div>
                             </div>
@@ -85,57 +88,55 @@ export default function Home({topics}) {
               </div>
             </div>
           </div>
-          <div>
-            <div class="mx-auto pt-12" style={{ width: "1100px" }}>
-              <div><h1 class="text-4xl text-center mb-6" style={{fontFamily: "futura_light_bt"}}>BRAND</h1></div>
-                <div class="flex">
-                  <div class="pb-4" style={{ width: "1100px"}}>
-                    <Image class="object-cover" src="/images/brand/brand_ba1_ls.jpg" width={1100} height={540} />
-                  </div>
-                </div>
-                <div class="flex">
-                  <div class="pb-4" style={{ width: "540px", marginRight: "20px" }}>
-                    <Image class="object-cover" src="/images/brand/brand_lu1_2.jpg" width={540} height={324} />
-                  </div>
-                  <div class="pb-4" style={{ width: "540px"}}>
-                    <Image src="/images/brand/allrightleather.jpg" width={540} height={324} />
-                  </div>
-                </div>
+
+          <div class="mx-auto pt-12 lg:w-1100">
+            <h1 class="text-4xl text-center mb-6" style={{fontFamily: "futura_light_bt"}}>BRAND</h1>
+            <div class="flex">
+              <div class="pb-4" style={{ width: "1100px"}}>
+                <Image class="object-cover" src="/images/brand/brand_ba1_ls.jpg" width={1100} height={540} />
               </div>
+            </div>
+            <div class="flex">
+              <div class="pb-4 mr-5">
+                <Image class="object-cover" src="/images/brand/brand_lu1_2.jpg" width={540} height={324} />
+              </div>
+              <div class="pb-4" style={{ width: "540px"}}>
+                <Image src="/images/brand/allrightleather.jpg" width={540} height={324} />
+              </div>
+            </div>
           </div>
-          <div>
-            <div class="mx-auto pt-12" style={{ width: "1100px" }}>
-              <div><h1 class="text-4xl text-center mb-6" style={{fontFamily: "futura_light_bt"}}>MEDIA</h1></div>
-              <div  class="relative">
-                <div class="flex absolute z-10" style={{top: "50%", left: "50%", transform: "translate(-52%, 0%)"}}>
-                  <div class="inline-block">
-                    <a href="https://www.instagram.com/masaki.s_japan/">
-                      <Image src="/images/media/instagram_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
-                    </a>
-                  </div>
-                  <div class="inline-block">
-                    <a href="https://www.facebook.com/masaki.s_japan/">
-                      <Image src="/images/media/facebook_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
-                    </a>
-                  </div>
-                  <div class="inline-block">
-                    <a href="https://www.youtube.com/masaki.s_japan/">
-                      <Image src="/images/media/youtube_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
-                    </a>
-                  </div>
-                  <div class="inline-block">
-                    <a href="https://www.twitter.com/masaki.s_japan/">
-                      <Image src="/images/media/twitter_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
-                    </a>
-                  </div>
+
+          <div class="mx-auto pt-12 lg:w-1100">
+            <h1 class="text-4xl text-center mb-6" style={{fontFamily: "futura_light_bt"}}>MEDIA</h1>
+            <div  class="relative">
+              <div class="flex absolute z-10" style={{top: "50%", left: "50%", transform: "translate(-52%, 0%)"}}>
+                <div class="inline-block">
+                  <a href="https://www.instagram.com/masaki.s_japan/">
+                    <Image src="/images/media/instagram_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
+                  </a>
                 </div>
-                <div class="absolute z-10" style={{top: "50%", left: "50%", transform: "translate(-52%, -92%)"}}>
-                  <Link href="/media">
-                  <a><Image src="/images/logo/falconer_co_ltd_logo.jpg" width={456} height={95} alt="falconer_co_ltd_logo" /></a>
-                  </Link>
+                <div class="inline-block">
+                  <a href="https://www.facebook.com/masaki.s_japan/">
+                    <Image src="/images/media/facebook_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
+                  </a>
                 </div>
-                <Image src="/images/media/top-media.jpg" width={1100} height={620} />
+                <div class="inline-block">
+                  <a href="https://www.youtube.com/masaki.s_japan/">
+                    <Image src="/images/media/youtube_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
+                  </a>
+                </div>
+                <div class="inline-block">
+                  <a href="https://www.twitter.com/masaki.s_japan/">
+                    <Image src="/images/media/twitter_white_bg_bk.jpg" width={114} height={114} alt="instagaram_social" />
+                  </a>
+                </div>
               </div>
+              <div class="absolute z-10" style={{top: "50%", left: "50%", transform: "translate(-52%, -92%)"}}>
+                <Link href="/media">
+                <a><Image src="/images/logo/falconer_co_ltd_logo.jpg" width={456} height={95} alt="falconer_co_ltd_logo" /></a>
+                </Link>
+              </div>
+              <Image src="/images/media/top-media.jpg" width={1100} height={620} />
             </div>
           </div>
           <div>
