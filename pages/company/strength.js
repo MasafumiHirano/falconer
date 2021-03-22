c8bd3a2546f56c13ef57780ce99421486c921684
katsuhiko-ranku <k.fuse@ranku.jp>
2021-03-22 14:05:22 +0900

    media,strengthレスポンシブ対応

--- pages/company/strength.js
+++ pages/company/strength.js
@@ -11,26 +11,20 @@ export default function Strength() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-      <main class="py-12" style={{ backgroundColor: "#ffffff" }}>
-        <div class="mx-auto p-12 mb-12 bg-white" style={{ width: "1100px" }}>
-          <div><h1 class="text-4xl text-center mb-36">社の強み</h1></div>
-          <div class="my-12">
-            <div><h1 class="text-4xl text-center font-bold mb-24">最高品質の戦略的モノ作りカンパニー</h1></div>
-            <p>メーカー・卸問屋・小売店。全ての機能を併せ持ち、マーケットの求める製品を世界から生産・調達。</p>
-            <p>{`　`}</p>
-            <p>FALCONERが担うのは製造だけではありません。</p>
-            <p>{`　`}</p>
-            <p>製造メーカー・卸元・小売店舗の役割まで一括して担い、マーケットからの声をダイレクトに活かしながら、業種にとらわれずスピード感を持ってマーケットの重要に対応する商品を開発する事を可能にしました。
-  日々、多様化するニーズに対応するため、素材・カテゴリー・技術・素材の枠組みを超え、複数の軸での商品開発を骨子に戦略的なモノ作りを行って参ります。</p>
-            <p>{`　`}</p>
+      <main class="lg:py-12" style={{ backgroundColor: "#ffffff" }}>
+        <div class="mx-auto  bg-white px-4 lg:px-12 mb-8 lg:mb-24 lg:w-1100">
+          <h1 class="text-center text-2xl mt-4 lg:text-4xl lg:mt-12">社の強み</h1>
+          <div class="my-8 lg:mt-36 lg:mb-12">
+            <h1 class="text-center font-bold text-2xl mb-6 lg:text-4xl lg:mb-24">最高品質の戦略的モノ作りカンパニー</h1>
+            <p class="text-sm lg:text-base mb-6">メーカー・卸問屋・小売店。全ての機能を併せ持ち、マーケットの求める製品を世界から生産・調達。</p>
+            <p class="text-sm lg:text-base mb-6">FALCONERが担うのは製造だけではありません。</p>
+            <p class="text-sm lg:text-base">製造メーカー・卸元・小売店舗の役割まで一括して担い、マーケットからの声をダイレクトに活かしながら、業種にとらわれずスピード感を持ってマーケットの重要に対応する商品を開発する事を可能にしました。日々、多様化するニーズに対応するため、素材・カテゴリー・技術・素材の枠組みを超え、複数の軸での商品開発を骨子に戦略的なモノ作りを行って参ります。</p>
           </div>
-          <div class="flex justify-center mb-24">
+          <div class="flex justify-center mb-12 lg:mb-24">
             <Image class="mx-auto inline-block" src="/images/company/spc_system.png" width={550} height={776} alt="spc_system"/>
           </div>
-          <p>お客様へお届けするまでのすべての業務を自社で行い、世界各地を生産拠点・販売拠点にしながら商品ごとの品質・技術管理を行っています。</p>
-          <p>{`　`}</p>
-          <p>様々な枠組みを超え、不要なこだわりを持たず、幅広い素材を広い角度から制約の無いモノ作りを展開して参ります。</p>
-          <p>{`　`}</p>
+          <p class="text-sm lg:text-base mb-6">お客様へお届けするまでのすべての業務を自社で行い、世界各地を生産拠点・販売拠点にしながら商品ごとの品質・技術管理を行っています。</p>
+          <p class="text-sm lg:text-base">様々な枠組みを超え、不要なこだわりを持たず、幅広い素材を広い角度から制約の無いモノ作りを展開して参ります。</p>
         </div>
       </main>
       </Layout>
--- pages/media/index.js
+++ pages/media/index.js
@@ -21,88 +21,83 @@ export default function Media({videos}) {
       <Layout>
       <main>
         <div style={{backgroundColor: "#786b58"}}>
-          <div class="mx-auto pt-12 pb-12" style={{ width: "1100px" }}>
+          <div class="mx-auto py-12 lg:w-1100">
             {/*見出し部分*/}
-            <div class="flex pb-2 border-b-2 border-solid border-white mb-12 items-center">
-              <div class="w-1/4"><p>{`　`}</p></div>
-              <div class="w-2/4">
-                <h1 class="text-7xl text-center text-white futura-co">MEDIA</h1>
-                </div>
-              <div class="w-1/4">
-                <div class="float-right mr-4">
-                  <div class="inline-block">
-                    <a href="https://www.instagram.com/masaki.s_japan/">
-                      <Image src="/images/media/instagram_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" />
-                    </a>
+              <div class="border-b-2 border-solid border-white mb-4 lg:mb-12">
+                <div class="relative">
+                  <h1 class="text-center text-white futura-co text-4xl lg:text-7xl pb-2">MEDIA</h1>
+                  <div class="absolute right-0 top-3 lg:top-5">
+                    <div class="block flex pr-2 w-20 lg:w-full">
+                      <div class="">
+                        <a href="https://www.instagram.com/masaki.s_japan/">
+                          <Image src="/images/media/instagram_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" />
+                        </a>
+                      </div>
+                      <div class=""><Image src="/images/media/youtube_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                      <div class=""><Image src="/images/media/twitter_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
+                    </div>
                   </div>
-                  <div class="inline-block"><Image src="/images/media/youtube_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
-                  <div class="inline-block"><Image src="/images/media/twitter_white_bg_bk.jpg" width={36} height={36} alt="instagaram_social" /></div>
                 </div>
               </div>
-            </div>
             {/*見出し部分 終了*/}
+
             {/*内容*/}
-            <div class="px-6 pt-8 bg-white border-2 border-solid border-black">
+            <div class="bg-white border-2 border-solid border-black px-4 pt-4 lg:px-6 lg:pt-8">
               <section class="mb-24">{/*RadioSection*/}
-                <h1 class="text-5xl text-center mb-6 helvetica font-semibold" style={{fontSize: "2.5rem"}}>RADIO</h1>
-                <div class="mb-6">
+                <h1 class="text-center helvetica font-semibold text-2xl mb-2 lg:text-5xl lg:mb-6">RADIO</h1>
+                <div class="mb-2 lg:mb-6">
                   <Image src="/images/media/bond_jam.jpg" alt="radio-image" width={1100} height={227} />
                 </div>
-                <div class="mx-auto w-5/6">
-                  <p class="chuGo text-xl mb-4 tracking-wide">MC: NANAMI 清水真輝</p>
-                  <p class="chuGo text-xl mb-4 tracking-wide">BACHIC ASHLEY がプロデュースする日常を最高の気分で過ごすために毎日を日々、</p>
-                  <p class="chuGo text-xl mb-4 tracking-wide">力強く生きているアーティスト、クリエイター、経営者など、各界の最先端で活躍する方々を</p>
-                  <p class="chuGo text-xl mb-6 tracking-wide">ゲストとしてお招きし、その強さの秘訣とこだわりの人生についてお聞きします。</p>
-                  <p class="chuGo text-lg">FM 愛知　毎週20：00～20：30　OA</p>
+                <div class="mx-auto w-5/6 chuGo lg:tracking-wide">
+                  <p class="text-sm lg:text-xl mb-4">MC: NANAMI 清水真輝</p>
+                  <div class="text-sm lg:text-xl">
+                    <p class="leading-6 lg:leading-10 mb-4">BACHIC ASHLEY がプロデュースする日常を最高の気分で過ごすために毎日を日々、力強く生きているアーティスト、クリエイター、経営者など、各界の最先端で活躍する方々をゲストとしてお招きし、その強さの秘訣とこだわりの人生についてお聞きします。</p>
+                  </div>
+                  <p class="text-xs lg:text-lg">FM 愛知　毎週20：00～20：30　OA</p>
                 </div>
               </section>{/*RadioSectionEnd*/}
+
               <section class="mb-24">{/*YoutubeSection*/}
-                <div class="flex justify-center mb-6">
-                  <div class="flex items-center">
-                    <h1 class="text-5xl text-center mr-6 helvetica font-semibold" style={{fontSize: "2.5rem"}}>YouTube Channel</h1>
+                <div class="lg:flex justify-center lg:mb-6">
+                  <h1 class="helvetica font-semibold text-center text-2xl lg:text-5xl lg:mr-6">YouTube Channel</h1>
+                  <div class="pt-4 w-1/4 lg:pt-0 lg:w-auto lg:flex items-center">
                     <Image src="/images/media/youtube-full-logo_lg.png" width={150} height={34} alt="youtubelogo"/>
                   </div>
                 </div>
-                <div class="flex pt-6 pb-6 mb-4" style={{ backgroundColor: "#221916" }}>
-                  <div class="w-2/4">
-                    <div class="flex justify-center items-center h-full">
-                      <iframe width="95%" height="95%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="px-6 py-6 mb-4" style={{ backgroundColor: "#221916" }}>
+                  <div class="w-full grid grid-rows-5 grid-cols-1 lg:grid-rows-2 lg:grid-cols-4 gap-4">
+                    <div class="lg:col-span-2 lg:row-span-2">
+                      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/CmNI0Xe8OoU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                  </div>
-                  <div class="w-1/4">
-                    <div class="flex justify-center items-center h-1/2">
-                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/DwZE9DUvGss" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="">
+                      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/DwZE9DUvGss" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <div class="flex justify-center items-center h-1/2">
-                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/k5oSVWFyOqE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="">
+                      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/z43qiLQsTjM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                  </div>
-                  <div class="w-1/4">
-                    <div class="flex justify-center items-center h-1/2">
-                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/z43qiLQsTjM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="">
+                      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/k5oSVWFyOqE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
-                    <div class="flex justify-center items-center h-1/2">
-                      <iframe width="90%" height="90%" src="https://www.youtube.com/embed/bY65l5fmnhU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="">
+                      <iframe width="100%" height="100%" src="https://www.youtube.com/embed/bY65l5fmnhU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                   </div>
                 </div>
-                <div class="mx-auto w-5/6">
-                  <p class="chuGo text-xl mb-4">BACHIC ASHLEY がプロデュースするラジオ番組「Bond Jam」のアーカイブからブランド、</p>
-                  <p class="chuGo text-xl mb-4">企業情報まで株式会社ファルコナーの魅力をYouTube コンテンツで発信します。</p>
+                <div class="mx-auto w-5/6 chuGo lg:tracking-wide text-sm lg:text-xl">
+                  <p class="leading-6 lg:leading-10">BACHIC ASHLEYがプロデュースするラジオ番組「Bond Jam」のアーカイブからブランド、企業情報まで株式会社ファルコナーの魅力をYouTubeコンテンツで発信します。</p>
                 </div>
               </section>{/*YoutubeSectionEnd*/}
+
               <section class="mb-24">{/*SNSSection*/}
-                <h1 class="text-5xl text-center mr-6 helvetica font-semibold mb-16" style={{fontSize: "2.5rem"}}>SNS</h1>
-                <div class="flex">
-                  <div class="w-1/2 pl-12">
-                    <div class="flex justify-start">
-                      <Image class="block" src="/images/media/instagram_logo_1500.png" width={40} height={40} alt="twitter_logo" />
-                    </div>
-                    <div class="mt-6">
+                <h1 class="text-center helvetica font-semibold text-2xl mb-2 lg:text-5xl lg:mb-6">SNS</h1>
+                <div class="lg:flex justify-around">
+                  <div class="block">
+                    <Image class="block" src="/images/media/instagram_logo_1500.png" width={40} height={40} alt="twitter_logo" />
+                    <div class="mt-2 lg:mt-6 text-center">
                       <InstagramEmbed
                         url='https://www.instagram.com/p/CMO-EpVgQDh/'
                         clientAccessToken={clientAccessToken}
-                        maxWidth={400}
+                        maxWidth={700}
                         hideCaption={true}
                         containerTagName='div'
                         protocol=''
@@ -113,29 +108,27 @@ export default function Media({videos}) {
                         onFailure={() => {}}
                       />
                     </div>
-                    <div class="mt-12">
+                    <div class="mt-12 text-center">
                       <p>公式インスタグラムはこちら</p>
                         <a href="https://www.instagram.com/masaki.s_japan/" target="_blank">
-                          <Image src="/images/media/instagram_banner.png" width={400} height={120} alt="instagram-banner"/>
+                          <Image src="/images/media/instagram_banner.png" width={300} height={90} alt="instagram-banner"/>
                         </a>
                     </div>
                   </div>
-                  <div class="w-1/2 pr-12">
-                    <div class="flex justify-end">
-                      <Image class="block" src="/images/media/Twitter_Logo_WhiteOnBlue_400.png" width={40} height={40} alt="twitter_logo" />
-                    </div>
-                    <div class="flex justify-end mt-6">
-                      <div style={{ width: "400px" }}>
+
+                  <div class="block mt-6 lg:mt-0">
+                    <Image class="block" src="/images/media/Twitter_Logo_WhiteOnBlue_400.png" width={40} height={40} alt="twitter_logo" />
+                    <div class="mt-2 lg:mt-6 lg:w-400">
                         <TwitterTimelineEmbed
                           sourceType="profile"
                           screenName="Nanami_Bondjam"
                           options={{height: 800}}
                         />
-                      </div>
                     </div>
                   </div>
                 </div>
               </section>{/*SNSSectionEnd*/}
+
             </div>
           </div>{/*コンテナー終了*/}
         </div>
