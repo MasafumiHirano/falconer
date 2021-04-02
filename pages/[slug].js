57b0fefb9e5824fa7f5d34e2032cca85d5b8fd5b
ranku-dev <develop@ranku.jp>
2021-04-02 18:46:00 +0900

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 export default function draftTopic({ topic }) {
   return (
-    !topic ?
+    (topic == undefined) ?
     <>
      <div>no topic</div>
     </>
