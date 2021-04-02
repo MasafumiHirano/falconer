e8d5552f45bba5ae7b338b6c6496e718a15b6668
ranku-dev <develop@ranku.jp>
2021-04-02 16:22:25 +0900

    update

--- pages/[slug].js
+++ pages/[slug].js
@@ -7,6 +7,21 @@ export default function draftPage({ content }) {
   );
 }
 
+export const getStaticPaths = async (context) => {
+  const slug = context.params.slug
+  const draftKey = context.previewData.draftKey
+  const key = {
+    headers: {'X-API-KEY': process.env.api_key},
+  };
+  const data = await fetch(`https://falconer.microcms.io/api/v1/topics/${slug}${
+    draftKey !== undefined ? `?draftKey=${draftKey}` : ''
+   }`, key)
+    .then(res => res.json())
+    .catch(() => null);
+  const paths = data.contents.map(content => `/${content.id}`);
+  return {paths, fallback: true};
+};
+
 export const getStaticProps = async (context) => {
   const slug = context.params.slug;
   const draftKey = context.previewData.draftKey;
@@ -25,19 +40,4 @@ export const getStaticProps = async (context) => {
        content
      }
    };
- };
-
- export const getStaticPaths = async (context) => {
-  const slug = context.params.slug
-  const draftKey = context.previewData.draftKey
-  const key = {
-    headers: {'X-API-KEY': process.env.api_key},
-  };
-  const data = await fetch(`https://falconer.microcms.io/api/v1/topics/${slug}${
-    draftKey !== undefined ? `?draftKey=${draftKey}` : ''
-   }`, key)
-    .then(res => res.json())
-    .catch(() => null);
-  const paths = data.contents.map(content => `/${content.id}`);
-  return {paths, fallback: true};
-};
+ }; 
