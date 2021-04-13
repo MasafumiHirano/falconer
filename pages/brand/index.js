11efc9e93564a9561022cf6f90462c61beb2f485
ranku-dev <develop@ranku.jp>
2021-04-13 19:34:51 +0900

    update

--- pages/brand/index.js
+++ pages/brand/index.js
@@ -16,7 +16,7 @@ export default function Brand() {
             <h1 class="Osaka font-bold text-3xl text-center mb-6 py-2 bg-falGLD text-white">ブランド</h1>
             <div class="block mx-auto lg:flex lg:px-0 mt-6 mb-6 lg:mt-12">
               <div class="lg:w-1/2 relative w-full">
-                <Image className="object-cover" src="/images/brand/brand-ba.jpg" alt="bachicashley" width={1100} height={550} />
+                <Image className="object-cover" src="/images/brand/brand-ba2.jpg" alt="bachicashley" width={1100} height={550} />
                 <p class="mt-2 lg:mt-6 px-2">BachicAshleyは、人生における全ての時間を幸せに過ごしていくために、「最上級の上質さ」を提供します。</p>
               </div>
               <div class="lg:w-1/2 relative w-full lg:ml-8 mt-12 lg:mt-0">
