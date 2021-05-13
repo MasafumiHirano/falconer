8c605a5b42b4e32d9cfcc890f2de9b39bdf766c3
katsuhiko-ranku <k.fuse@ranku.jp>
2021-05-13 19:28:21 +0900

    #15 修正（一部未完）

--- components/header.js
+++ components/header.js
@@ -3,9 +3,30 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react';
 import Menu from './menu';
- 
-export default function Header(){
+import { useState } from "react";
 
+
+export default function Header() {
+
+  const [scroll, setScroll] = useState("");
+  const [header, setHeader] = useState("header");
+  const [header__nav, setHeaderNav] = useState("header__nav");
+
+
+  if (process.browser) {
+    window.addEventListener( "scroll" ,function(){
+      //スクロールの高さを取得
+      setScroll(window.pageYOffset)
+      if(scroll > 100) {
+        setHeader("header_change");
+        setHeaderNav("header__nav_change");
+      } else {
+        setHeader("header");
+        setHeaderNav("header__nav");
+      }
+    })
+  }
+  
   return (
     <>
     <Head>
@@ -22,9 +43,90 @@ export default function Header(){
         crossOrigin=""
       />
     </Head>
+    
+    <div className={`${header}`}>
+      <div className="header__inner">
+        
+        <div class="w-3/12 pt-2 pl-5 flex items-center lg:hidden">
+          <Menu />
+        </div>
 
-      <div class="w-full bg-black z-20 fixed top-0 inset-x-1/2 transform -translate-x-1/2 -ml-px">
-        <div class="mx-auto flex lg:w-1280 bg-black">
+        <div class="w-1/2 lg:w-3/12">
+          <div class="block pt-2">
+            <Link href="/">
+              <a><Image src="/images/logo/falconer_co_ltd_logo.jpg" width={400} height={84} alt="falconer_logo" /></a>
+            </Link>
+          </div>
+        </div>
+
+        <ul class="w-7/12 hidden lg:flex lg:justify-end lg:items-center text-xs lg:text-base">
+          <li>
+            <Link href="/topics">
+              <a className={`${header__nav}`}>
+                最新情報
+              </a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/brand">
+            <a className={`${header__nav}`}>
+                ブランド
+              </a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/company">
+            <a className={`${header__nav}`}>
+                企業情報
+              </a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/media">
+            <a className={`${header__nav}`}>
+                メディア
+              </a>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/contact">
+            <a className={`${header__nav}`}>
+                お問い合わせ
+              </a>
+            </Link>
+          </li>
+
+        </ul>
+
+        <div class="w-1/4 lg:w-2/12 flex justify-end items-center">
+          <div class="flex invisible lg:visible">
+            <div class="flex items-center px-2">
+              <a class="pt-1" href="https://www.instagram.com/masaki.s_japan/" target="_black">
+                <Image src="/images/media/instagram_white_bg_bk.jpg" width={48} height={48} alt="instagaram_social" />
+              </a>
+            </div>
+            <div class="flex items-center px-2">
+              <a class="pt-1" href="https://www.youtube.com/channel/UCynRnKX32Zr8Hh_Rst6Mrug" target="_black">
+                <Image src="/images/media/youtube_white_bg_bk.jpg" width={48} height={48} alt="youtube_social" />
+              </a>
+            </div>
+            <div class="flex items-center px-2">
+              <a class="pt-1" href="https://twitter.com/Nanami_Bondjam" target="_blank">
+                <Image src="/images/media/twitter_white_bg_bk.jpg" width={48} height={48} alt="twitter_social" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      {/* <div class="w-full bg-black z-20 fixed top-0 inset-x-1/2 transform -translate-x-1/2 -ml-px">
+        <div class="mx-auto flex lg:w-1280">
+          
           <div class="w-3/12 pt-2 pl-5 flex items-center lg:hidden">
             <Menu />
           </div>
@@ -85,7 +187,9 @@ export default function Header(){
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
+
+
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,7 @@ import Layout from '../components/layout'
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
-export default function Home({slides,topics}) {
-  //slider設定
+export default function Home({slides,topics,readings}) {  //slider設定
   const settings = {
     dots: false,
     infinite: true,
@@ -23,7 +22,51 @@ export default function Home({slides,topics}) {
     fade: false,
     arrows: true,
     className: 'slides'
-};
+  };
+
+  const renderSlider = (slides) => {
+    return (
+    slides.filter((slide)=>(
+      slide.visible == true
+    ))
+    .map((slide)=> (
+      slide.topics_ref ?
+      slide.topics_ref.main_image_sp ?
+      <div>
+        <Link href={`/topics/${slide.topics_ref.id}`}><a>
+        <picture>
+          <source media="(min-width: 960px)" srcset={slide.topics_ref.main_image.url} />
+          <img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.topics_ref.main_image_sp.url} />
+        </picture>
+        {/*<img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.topics_ref.main_image_sp.url} width={1280} height={640} alt="bachicashley_hero"/>*/}
+        </a></Link>
+      </div>
+      :
+      <div>
+        <Link href={`/topics/${slide.topics_ref.id}`}><a>
+          <img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.topics_ref.main_image.url} width={1280} height={640} alt="bachicashley_hero"/>
+        </a></Link>
+      </div>
+      :
+      slide.slider_image_sp ?
+      <div>
+        <a href={slide.link}>
+        <picture>
+          <source media="(min-width: 960px)" srcset={slide.slider_image.url} />
+          <img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.slider_image_sp.url} />
+        </picture>
+          {/*<img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.slider_image_sp.url} width={1280} height={640} alt="bachicashley_hero"/>*/}
+        </a>
+      </div>
+      :
+      <div>
+        <a href={slide.link}>
+          <img className="inline-block mx-auto w-full h-screen lg:h-auto object-cover" src={slide.slider_image.url} width={1280} height={640} alt="bachicashley_hero"/>
+        </a>
+      </div>
+    ))
+    )
+  }
 
   return (
     <div>
@@ -36,79 +79,69 @@ export default function Home({slides,topics}) {
           {/* TOPスライダーセクション START */}
           <div class="mx-auto">
             <Slider {...settings}>
-              {
-                slides.filter((slide)=>(
-                  slide.visible == true
-                ))
-                .map((slide)=> (
-                  slide.topics_ref ? 
-                  <div>
-                    <Link href={`/topics/${slide.topics_ref.id}`}><a>
-                    <img className="inline-block mx-auto w-full" src={slide.topics_ref.main_image.url} width={1280} height={640} alt="bachicashley_hero"/>
-                    </a></Link>
-                  </div>
-                  :
-                  <div>
-                    <a href={slide.link}>
-                      <img className="inline-block mx-auto w-full" src={slide.slider_image.url} width={1280} height={640} alt="bachicashley_hero"/>
-                    </a>
-                  </div>
-                ))
-              }
+              {renderSlider(slides)}
             </Slider>
           </div>
           {/* TOPスライダーセクション END */}
 
           {/* 最新情報セクション START */}
-          <div class="mx-auto py-6 lg:py-12 lg:w-1100">
-            <Link href="/topics">
-              <a><h1 class="Osaka font-bold text-xl lg:text-3xl text-center mb-2 py-2 tracking-wider">新着情報</h1></a>
-            </Link>
-            <p class="text-center mb-2">ファルコナーの最新の情報をお届けします。</p>
-            <div class="overflow-x-scroll">
-              <ul class="flex w-920 lg:w-full">
-                {
-                topics.filter(topic => (
-                    topic.toppage == true
-                  ))
-                .slice(0,3)
-                .map(topic => (
-                  <li key={topic.id} class="shadow hover:bg-gray-100 m-2">
-                    <Link href={`topics/${topic.id}`}><a>
-                      <Image src={`${topic.main_image.url}`} width={1100} height={550} alt=""/>
-                      <div class="p-2 h-14 lg:h-20 lg:mb-2 overflow-hidden">
-                        <p class="font-semibold helvetica" style={{fontSize: "17px"}}>{topic.title}</p>
-                      </div>
-                      <div class="p-2 lg:flex lg:pt-0 flex-wrap">
-                        {topic.tag.map(tag => (
-                          <div class="inline-block mt-2 mr-2 mb-4">
-                            <span class="px-2 lg:px-4 py-1 lg:py-2 mb-2 bg-gray-200 rounded-full text-xs whitespace-nowrap">{tag.tagname}</span>
-                          </div>
-                        ))}
-                      </div>
-                    </a></Link>
-                  </li>
-                ))}
-              </ul>
+          <div>
+            <div class="mx-auto py-6 lg:py-12 lg:w-1100">
+              <Link href="/topics">
+                <a><h1 class="Osaka font-bold text-xl lg:text-3xl text-center mb-2 py-2 tracking-wider">新着情報</h1></a>
+              </Link>
+              <p class="text-center mb-2">ファルコナーの最新の情報をお届けします。</p>
+              <div class="overflow-x-auto">
+                <div class="w-1100">
+                  <ul class="grid grid-cols-3 gap-x-3 gap-y-6 lg:gap-x-4 lg:gap-y-12">
+                    {
+                    topics.filter(topic => (
+                        topic.toppage == true
+                      ))
+                    .slice(0,3)
+                    .map(topic => (
+                      <li key={topic.id} class="shadow hover:bg-gray-100 my-2">
+                        <Link href={`topics/${topic.id}`}>
+                          <a>
+                            <div class="px-2 lg:px-0">
+                              <div>
+                                <Image src={`${topic.main_image.url}`} width={1100} height={550} alt=""/>
+                              </div>
+                              <div class="p-2 h-14 lg:h-20 lg:mb-2 overflow-hidden">
+                                <p class="font-semibold helvetica" style={{fontSize: "17px"}}>{topic.title}</p>
+                              </div>
+                              <div class="p-2 lg:flex lg:pt-0 flex-wrap">
+                                {topic.tag.map(tag => (
+                                  <div class="inline-block mt-2 mr-2 mb-4">
+                                    <span class="px-2 lg:px-4 py-1 lg:py-2 mb-2 bg-gray-200 rounded-full text-xs whitespace-nowrap">{tag.tagname}</span>
+                                  </div>
+                                ))}
+                              </div>
+                            </div>
+                          </a>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
           {/* 最新情報セクション END */}
 
-           {/* 企業情報セクション START */}
-           <div
+          {/* 企業情報セクション START */}
+          <div
             class="bg-center"
             style={{ backgroundImage: "url(../images/top/top_section_company_bg1.jpg)"}}
-            >
+          >
             <div class="mx-auto py-6 mb-6 lg:mb-12 lg:py-12 lg:w-1100">
               <Link href="/company">
                 <a><h1 class="Osaka font-bold text-xl lg:text-3xl text-center mb-2 py-2 tracking-wider">企業情報</h1></a>
               </Link>
-                <p class="text-center mb-2">ファルコナーの情報はこちらをご確認ください。</p>
-                <div class="bg-black flex justify-center py-12">
-                <Link href="/company">
-                  <a><Image src="/images/logo/falconer-logo_borderwhite.png" width={500} height={104} alt="falconerinc_logo"/></a>
-                </Link>
-              </div>
+              <p class="text-center mb-2">ファルコナーの情報はこちらをご確認ください。</p>
+              <Link href="/company">
+                <a><Image src="/images/top/image_1100x200.png" width={1100} height={200} alt="company_info"/></a>
+              </Link>
             </div>
           </div>
           {/* 企業情報セクション END */}
@@ -225,6 +258,38 @@ export default function Home({slides,topics}) {
                   <a><Image src="/images/media/brandstory.png" width={1100} height={620} /></a>
                 </Link>
               </div>
+
+              {/* 読み物セクション START */}
+              <div class="mx-auto py-6 lg:py-12 lg:w-1100">
+                <Link href="/readings">
+                  <a><h1 class="Osaka font-bold text-lg lg:text-xl text-center mb-2 py-2 tracking-wider">読み物</h1></a>
+                </Link>
+                <div class="overflow-x-scroll">
+                  <ul class="flex w-920 lg:w-full">
+                    {
+                    readings
+                    .slice(0,3)
+                    .map(reading => (
+                      <li key={reading.id} class="shadow hover:bg-gray-100 m-2">
+                        <Link href={`readings/${reading.id}`}><a>
+                          <Image src={`${reading.main_image.url}`} width={1100} height={550} alt=""/>
+                          <div class="p-2 h-14 lg:h-20 lg:mb-2 overflow-hidden">
+                            <p class="font-semibold helvetica" style={{fontSize: "17px"}}>{reading.title}</p>
+                          </div>
+                          <div class="p-2 lg:flex lg:pt-0 flex-wrap">
+                            {reading.tag.map(tag => (
+                              <div class="inline-block mt-2 mr-2 mb-4">
+                                <span class="px-2 lg:px-4 py-1 lg:py-2 mb-2 bg-gray-200 rounded-full text-xs whitespace-nowrap">{tag.tagname}</span>
+                              </div>
+                            ))}
+                          </div>
+                        </a></Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              {/* 読み物セクション END */}
             </div>
           </div>
           {/* メディアセクション END */}
@@ -273,10 +338,14 @@ export const getStaticProps = async () => {
   const topics = await fetch('https://falconer.microcms.io/api/v1/topics', key)
     .then(res => res.json())
     .catch(() => null);
+  const readings = await fetch('https://falconer.microcms.io/api/v1/readings', key)
+    .then(res => res.json())
+    .catch(() => null);
   return {
     props: {
       slides: topslides.contents,
       topics: topics.contents,
+      readings: readings.contents,
     },
   };
 };
