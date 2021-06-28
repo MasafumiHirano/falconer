279f03d850762c6e45642bf97e712c268000f5e4
MasafumiHirano <82073171+MasafumiHirano@users.noreply.github.com>
2021-06-28 16:57:39 +0900

    media_20210628_2

--- pages/media/index.js
+++ pages/media/index.js
@@ -49,7 +49,7 @@ export default function Media({medias}) {
                 <p class="text-center helvetica font-semibold text-2xl mb-2 lg:text-base lg:mb-6 leading-5">ラジオ</p>
                 <div class="mb-2 lg:mb-6">
                   <a href="https://www.youtube.com/channel/UCynRnKX32Zr8Hh_Rst6Mrug" target="_blank">
-                  <Image src="/images/media/nanajam_image.png" alt="radio-image" width={1600} />
+                      <Image src="/images/media/nanajam_image.png" alt="radio-image" width={1600} height={500} minWidth={0} minheight={0}/>
                   </a>
                 </div>
                 <div class="mx-auto w-5/6 chuGo lg:tracking-wide">
