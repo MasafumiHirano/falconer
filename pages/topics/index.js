77a37699b6c7ad96eaffd2912fc02ba4b2dbb89f
MasafumiHirano <82073171+MasafumiHirano@users.noreply.github.com>
2021-12-20 16:34:24 +0900

    リンク先調整＆メディア一覧のページネーション修正

--- pages/topics/index.js
+++ pages/topics/index.js
@@ -61,10 +61,10 @@ export default function Topics({ topics, totalCount }) {
               <ul class="mx-auto w-11/12 md:w-full mt-12">
                 {topics.map(topic => (
                   <li key={topic.id} class="py-4 border-b">
-                    <Link href={`topics/${topic.id}`}>
-                      <a class="flex flex-wrap md:flex-row md:justify-center">
+                    <Link href={topic.link && topic.link}>
+                      <a class="flex flex-wrap md:flex-row md:justify-center" target="blank_">
                         <div class="w-28 md:w-32 md:text-center">{getTopicDate(`${topic.datetime}`)}</div>
-                        <div class={`${topic.category_color} rounded-2xl w-32 text-center`}>
+                        <div class={`${topic.category_color} rounded-2xl w-32 text-center max-h-6`}>
                           <span class="text-white NotoSans-L">{topic.category}</span>
                         </div>
                         <p class="w-full mt-2 md:mt-0 md:w-9/12 md:ml-4 NotoSans-L break-words">{topic.title}</p>
