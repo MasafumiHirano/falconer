a5aa82804f90d8788a688d47af3d7f99c9032323
MasafumiHirano <82073171+MasafumiHirano@users.noreply.github.com>
2022-01-21 18:04:31 +0900

    OEM一次アップ

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,31 @@ module.exports = {
       pointerEvents: ['disabled'],
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        ".text-shadow": {
+          textShadow: "0px 2px 3px white"
+        },
+        ".text-shadow-md": {
+          textShadow: "0px 3px 3px white"
+        },
+        ".text-shadow-lg": {
+          textShadow: "0px 5px 3px white"
+        },
+        ".text-shadow-xl": {
+          textShadow: "0px 7px 3px white"
+        },
+        ".text-shadow-2xl": {
+          textShadow: "0px 10px 3px white"
+        },
+        ".text-shadow-none": {
+          textShadow: "none"
+        }
+      };
+      addUtilities(newUtilities);
+    }
+  ],
   theme: {
     screens: {
       'sm': '640px',
