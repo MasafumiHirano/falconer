66fb4391d738ea61f178e54b23582bbadc2c8260
MasafumiHirano <82073171+MasafumiHirano@users.noreply.github.com>
2025-07-01 19:35:38 +0900

    7369上げ漏れ

--- pages/catalog/7363.js
+++ pages/catalog/7363.js
@@ -6,27 +6,37 @@ export default function catalog7363() {
   return (
     <div>
       <Head>
-        <title>otz7363</title>
+        <title>waa7363</title>
         <link rel="icon" href="/falconer_favicons.png" />
       </Head>
       <Layout>
         <main role="main" class="pt-8 NotoSans">
           <section class="flex flex-wrap justify-center p-7">
-            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-8 border-b-4 border-double border-black pb-2">商品カタログ</h2>
-            <p class="py-10 text-black-900 text-xl font-bold w-4/5">[RafiCaro] otz7363</p>
+            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-2 border-b-4 border-double border-black pb-4">[RafiCaro]</h2>
+            <p class="py-6 text-black-900 text-xl font-bold w-4/5 text-center">コインケース</p>
             <div class="w-5/12"><img src="/images/catalog/7363.jpg" /></div>
             <dl class="w-6/12 pl-8">
               <dt class="bg-red-200 px-4 flex items-center font-bold">型番</dt><dd class="pt-4 pb-7 ml-7">7363</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">売価</dt><dd class="pt-4 pb-7 ml-7">メーカー希望小売価格：¥2,200</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">ブランド</dt><dd class="pt-4 pb-7 ml-7">RafiCaro</dd>
-              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">【サイズ】縦 30mm × 横 75mm × 厚み 28mm　重さ 15g 【素材】 ・本革 シュリンクレザー</dd>
-              <dt class="bg-red-200 px-4 flex items-center font-bold">カラー</dt><dd class="pt-4 pb-7 ml-7">sorange (JAN:4589681976058)</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">縦 30mm × 横 75mm × 厚み 28mm ／ 重さ 15g</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">素材</dt><dd class="pt-4 pb-7 ml-7">本革 シュリンクレザー</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">カラー</dt>
+              <dd class="pt-4 pb-7 ml-7">sgreen (JAN:4589681976010) / sdustypink (JAN:4589681976027) / sgreige (JAN:4589681976034) / sblack (JAN:4589681976041) / sorange (JAN:4589681976058) / sblue (JAN:4589681976065) / syellow (JAN:4589681976072) / slightgrey (JAN:4589681982387)</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元</dt><dd class="pt-4 pb-7 ml-7">株式会社FALCONER (株式会社ファルコナー)</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元住所</dt><dd class="pt-4 pb-7 ml-7">〒465-0068　愛知県名古屋市名東区牧の里三丁目104番地の2</dd>
             </dl>
 
+            {/* カートボタン */}
             <div class="w-full text-center mt-4">
-              <a href="https://www.casebank.jp/CART/cart.php?ITM=otz7363&CNT=1&VAR1=sorange" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sorange をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sgreen" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sgreen をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sdustypink" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sdustypink をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sgreige" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sgreige をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sblack" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sblack をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sorange" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sorange をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=sblue" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">sblue をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=syellow" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">syellow をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7363_CL&CNT=1&VAR1=slightgrey" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">slightgrey をカートに追加</a>
             </div>
           </section>
         </main>
