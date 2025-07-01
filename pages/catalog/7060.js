713b26fd39a97c279a6f93df3293d9796bb832b5
MasafumiHirano <82073171+MasafumiHirano@users.noreply.github.com>
2025-07-01 22:02:12 +0900

    不足ページと更新漏れの反映

--- pages/catalog/7060.js
+++ pages/catalog/7060.js
@@ -6,27 +6,38 @@ export default function catalog7060() {
   return (
     <div>
       <Head>
-        <title>wab7060</title>
+        <title>waa7060</title>
         <link rel="icon" href="/falconer_favicons.png" />
       </Head>
       <Layout>
         <main role="main" class="pt-8 NotoSans">
           <section class="flex flex-wrap justify-center p-7">
-            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-8 border-b-4 border-double border-black pb-2">商品カタログ</h2>
-            <p class="py-10 text-black-900 text-xl font-bold w-4/5">[RafiCaro] wab7060</p>
+            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-2 border-b-4 border-double border-black pb-4">[RafiCaro]</h2>
+            <p class="py-6 text-black-900 text-xl font-bold w-4/5 text-center">ミニ財布</p>
             <div class="w-5/12"><img src="/images/catalog/7060.jpg" /></div>
             <dl class="w-6/12 pl-8">
               <dt class="bg-red-200 px-4 flex items-center font-bold">型番</dt><dd class="pt-4 pb-7 ml-7">7060</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">売価</dt><dd class="pt-4 pb-7 ml-7">メーカー希望小売価格：¥4,000</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">ブランド</dt><dd class="pt-4 pb-7 ml-7">RafiCaro</dd>
-              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">[サイズ] 縦 70mm × 横 90mm × 厚み 35mm 重さ 43g</dd>
-              <dt class="bg-red-200 px-4 flex items-center font-bold">カラー</dt><dd class="pt-4 pb-7 ml-7">brown (JAN:4589681964147)</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">縦 70mm × 横 90mm × 厚み 35mm ・重さ 43g</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">素材</dt><dd class="pt-4 pb-7 ml-7">牛革</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">カラー</dt>
+              <dd class="pt-4 pb-7 ml-7">
+                brown (JAN:4589681964147) / darkbrown (JAN:4589681964154) / navy (JAN:4589681964161) / red (JAN:4589681964185) / green (JAN:4589681964178) / dustyblue (JAN:4589681984237) / greige (JAN:4589681984244)
+              </dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元</dt><dd class="pt-4 pb-7 ml-7">株式会社FALCONER (株式会社ファルコナー)</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元住所</dt><dd class="pt-4 pb-7 ml-7">〒465-0068　愛知県名古屋市名東区牧の里三丁目104番地の2</dd>
             </dl>
 
+            {/* カートボタン */}
             <div class="w-full text-center mt-4">
-              <a href="https://www.casebank.jp/CART/cart.php?ITM=wab7060&CNT=1&VAR1=brown" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">brown をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=brown" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">brown をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=darkbrown" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">darkbrown をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=navy" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">navy をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=red" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">red をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=green" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">green をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=dustyblue" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">dustyblue をカートに追加</a>
+              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7060_CL&CNT=1&VAR1=greige" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">greige をカートに追加</a>
             </div>
           </section>
         </main>
--- pages/catalog/7146.js
+++ pages/catalog/7146.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-
 import React from "react";
 import Layout from '../../components/layout'
 
@@ -13,24 +12,34 @@ export default function catalog7146() {
       <Layout>
         <main role="main" class="pt-8 NotoSans">
           <section class="flex flex-wrap justify-center p-7">
-            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-8 border-b-4 border-double border-black pb-2">商品カタログ</h2>
-            <p class="py-10 text-black-900 text-xl font-bold w-4/5">[RafiCaro] 長財布 レディース 本革 牛革 シボ革 スリム ラウンドファスナー</p>
+            <h2 class="text-center w-1050 text-3xl font-bold mx-auto mt-3 mb-2 border-b-4 border-double border-black pb-4">[RafiCaro]</h2>
+            <p class="py-6 text-black-900 text-xl font-bold w-4/5 text-center">財布</p>
             <div class="w-5/12"><img src="/images/catalog/7146.jpg" /></div>
             <dl class="w-6/12 pl-8">
               <dt class="bg-red-200 px-4 flex items-center font-bold">型番</dt><dd class="pt-4 pb-7 ml-7">7146</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">売価</dt><dd class="pt-4 pb-7 ml-7">メーカー希望小売価格：¥6,900</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">ブランド</dt><dd class="pt-4 pb-7 ml-7">RafiCaro</dd>
-              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">縦 95mm × 横 200mm × 厚み 25mm・重さ 217g</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">サイズ</dt><dd class="pt-4 pb-7 ml-7">縦 95mm × 横 200mm × 厚み 25mm ・重さ 217g</dd>
+              <dt class="bg-red-200 px-4 flex items-center font-bold">素材</dt><dd class="pt-4 pb-7 ml-7">外側：牛革 シボ革 ／ 内側：ナイロン マイクロファイバ</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">カラー</dt>
-              <dd class="pt-4 pb-7 ml-7">ivory (JAN:4589681971077) / yellow (JAN:4589681975228)</dd>
+              <dd class="pt-4 pb-7 ml-7">
+                black (JAN:4589681963492) / red (JAN:4589681963508) / green (JAN:4589681963515) / orange (JAN:4589681963522) / ashgrey (JAN:4589681975808) / greige (JAN:4589681975815) / camel (JAN:4589681975822) / dustypink (JAN:4589681975839) / ivory (JAN:4589681975846) / yellow (JAN:4589681975228) / blue (JAN:4589681975211) / leafgreen (JAN:4589681976867) / olive (JAN:4589681976874) / bordeaux (JAN:4589681976881) / gold (JAN:4589681981847) / saffianoblack (JAN:4589681981854) / silver (JAN:4589681982592)
+              </dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元</dt><dd class="pt-4 pb-7 ml-7">株式会社FALCONER (株式会社ファルコナー)</dd>
               <dt class="bg-red-200 px-4 flex items-center font-bold">販売元住所</dt><dd class="pt-4 pb-7 ml-7">〒465-0068　愛知県名古屋市名東区牧の里三丁目104番地の2</dd>
             </dl>
 
             {/* カートボタン */}
             <div class="w-full text-center mt-4">
-              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7146&CNT=1&VAR1=ivory" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">ivory をカートに追加</a>
-              <a href="https://www.casebank.jp/CART/cart.php?ITM=waa7146&CNT=1&VAR1=yellow" class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1">yellow をカートに追加</a>
+              {["black","red","green","orange","ashgrey","greige","camel","dustypink","ivory","yellow","blue","leafgreen","olive","bordeaux","gold","saffianoblack","silver"].map((color) => (
+                <a
+                  key={color}
+                  href={`https://www.casebank.jp/CART/cart.php?ITM=waa7146_CL&CNT=1&VAR1=${color}`}
+                  class="inline-block bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded m-1"
+                >
+                  {color} をカートに追加
+                </a>
+              ))}
             </div>
           </section>
         </main>
